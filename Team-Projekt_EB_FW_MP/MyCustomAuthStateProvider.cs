4a95ec5bd6b7995a4f0a2b9c13b6056c7c0aea78
262626 <2626262626>
2025-11-18 11:53:33 +0100

    #1 Login-Page/MyCustom...

---
+++ Team-Projekt_EB_FW_MP/MyCustomAuthStateProvider.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace Team_Projekt_EB_FW_MP
+{
+
+	public class MyCustomAuthStateProvider : AuthenticationStateProvider
+	{
+		private ClaimsPrincipal _anonymous = new(new ClaimsIdentity());
+		// _anonymous - falls _currentUser null ist
+		private ClaimsPrincipal? _currentUser = null;
+
+
+		public override Task<AuthenticationState> GetAuthenticationStateAsync()
+		{
+			return Task.FromResult(new AuthenticationState(_currentUser ?? _anonymous));
+		}
+
+
+		public void Login(string username)
+		{
+			ClaimsIdentity identity = new([new Claim(ClaimTypes.Name, username)],
+					"MyCustomAuthType");  // von SPAA erfunden, keiner der Standardtypen
+
+			_currentUser = new ClaimsPrincipal(identity);
+			NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+		}
+
+
+		public void Logout()
+		{
+			_currentUser = null;
+			NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+		}
+	}
+}
