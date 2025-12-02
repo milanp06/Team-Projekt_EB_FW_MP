9519c74a48626918451dc6be27f8e8fba8086819
Florian Wiesauer <f.wiesauer@htlvb.at>
2025-12-02 11:42:27 +0100

    Merkmale, Properties und Konstruktor

--- DatenbankLib/Rating.cs
+++ DatenbankLib/Rating.cs
@@ -2,5 +2,27 @@
 {
     internal class Rating
     {
+        // Merkmale 
+        private static string table_friendsOfAward_Ranking = "friendsOfAward_Ranking";
+
+        // Properties
+        private int id { get; }
+        private string TopFavorit { get; }
+        private string Favorit1 { get; }
+        private string Favorit2 { get; }
+        private string Favorit3 { get; }
+        private string Favorit4 { get; }
+        private string Favorit5 { get; }
+        // Konstruktor
+        public Rating(int id, string topFavorit, string favorit1, string favorit2, string favorit3, string favorit4, string favorit5)
+        {
+            this.id = id;
+            TopFavorit = topFavorit;
+            Favorit1 = favorit1;
+            Favorit2 = favorit2;
+            Favorit3 = favorit3;
+            Favorit4 = favorit4;
+            Favorit5 = favorit5;
+        }
     }
 }
