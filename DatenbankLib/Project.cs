5e36860cd4b65a8fbdd041535c64f3a2aeb5e7f2
262626 <2626262626>
2026-02-10 11:27:45 +0100

    #Juryvoting

--- DatenbankLib/Project.cs
+++ DatenbankLib/Project.cs
@@ -14,10 +14,10 @@ namespace DatenbankLib
         public string Title { get; }
         public string Author { get; }
         public int Schulvoting { get; }
-		public int Publikumsvoting { get; }
-		public int Juryvoting { get; }
+        public int Publikumsvoting { get; }
+        public int Juryvoting { get; }
 
-		public Project(string id, string title, string author, int schulvoting, int publikumsvoting, int juryvoting)
+        public Project(string id, string title, string author, int schulvoting, int publikumsvoting, int juryvoting)
         {
             Id = id;
             Title = title;
@@ -26,29 +26,29 @@ namespace DatenbankLib
             Publikumsvoting = publikumsvoting;
             Juryvoting = juryvoting;
 
-		}
+        }
 
-		public static int DeleteAllProjects()
-		{
-			DbWrapperMySql wrappr = DbWrapperMySql.Wrapper;
+        public static int DeleteAllProjects()
+        {
+            DbWrapperMySql wrappr = DbWrapperMySql.Wrapper;
 
             string sql = $"DELETE FROM {table_friendsOfAward_Projects};";
             int count = 0;
 
-			try
-			{
-				count = wrappr.RunNonQuery(sql);
-			}
-			catch (Exception ex)
-			{
-				Console.WriteLine(ex.Message);
-				count = -1;
-			}
+            try
+            {
+                count = wrappr.RunNonQuery(sql);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                count = -1;
+            }
 
-			return count;
-		}
+            return count;
+        }
 
-		public static string AddProjects(Project[] projects)
+        public static string AddProjects(Project[] projects)
         {
             DbWrapperMySql wrappr = DbWrapperMySql.Wrapper;
 
@@ -58,8 +58,8 @@ namespace DatenbankLib
 
             foreach (Project project in projects)
             {
-				sql = $"INSERT INTO {table_friendsOfAward_Projects}(Title, Author) VALUES ('{project.Title}','{project.Author}');";
-				try
+                sql = $"INSERT INTO {table_friendsOfAward_Projects}(Title, Author) VALUES ('{project.Title}','{project.Author}');";
+                try
                 {
                     wrappr.RunNonQuery(sql);
                 }
@@ -72,35 +72,35 @@ namespace DatenbankLib
 
             if (errorCount > 0)
             {
-				errorMessage = $"Erfolgreich eingefügt: {projects.Count() - errorCount} / Fehler: {errorCount}";
-			}
+                errorMessage = $"Erfolgreich eingefügt: {projects.Count() - errorCount} / Fehler: {errorCount}";
+            }
 
             return errorMessage;
         }
 
         public static int GetProjectCount()
         {
-			DbWrapperMySql wrappr = DbWrapperMySql.Wrapper;
+            DbWrapperMySql wrappr = DbWrapperMySql.Wrapper;
             DataTable dt = new();
             string sql = $"SELECT Count(*) FROM {table_friendsOfAward_Projects};";
-			int count = 0;
+            int count = 0;
 
-			try
-			{
-				dt = wrappr.RunQuery(sql);
+            try
+            {
+                dt = wrappr.RunQuery(sql);
 
                 count = Convert.ToInt32(dt.Rows[0][0]);
-			}
-			catch (Exception ex)
-			{
-				Console.WriteLine(ex.Message);
-				count = -1;
-			}
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                count = -1;
+            }
 
-			return count; 
+            return count;
         }
 
-        public static string CalculatePublikumsvoting ()
+        public static string CalculatePublikumsvoting()
         {
             string errorMessage = "";
 
@@ -119,7 +119,7 @@ namespace DatenbankLib
                 votePoints[(rating.Favorit3) - 1] += 1;
                 votePoints[(rating.Favorit4) - 1] += 1;
                 votePoints[(rating.Favorit5) - 1] += 1;
-			}
+            }
 
             Console.WriteLine("\nVotes:");
             for (int i = 1; i < count; i++)
@@ -142,7 +142,7 @@ namespace DatenbankLib
             for (int i = 0; i < count; i++)
             {
                 totalPoints[i] = (votePoints[i] * 30.0) / maxPoints;
-                Console.WriteLine($"{i+1}: {totalPoints[i]} Punkte");
+                Console.WriteLine($"{i + 1}: {totalPoints[i]} Punkte");
             }
 
             DbWrapperMySql wrappr = DbWrapperMySql.Wrapper;
@@ -171,6 +171,79 @@ namespace DatenbankLib
             }
 
             return errorMessage;
-		}
+        }
+
+        public static string CalculateJuryvoting(List<string> projectNames)
+        {
+            string errorMessage = "";
+
+            List<JuryRating> ratings = DatenbankLib.JuryRating.GetAllJuryRatings();
+
+            int count = projectNames.Count();
+            if (count <= 0) return "Keine Projekte gefunden!";
+
+            int[] votePoints = new int[count];
+
+            foreach (JuryRating rating in ratings)
+            {
+                votePoints[(rating.Projekt1) - 1] += 2;
+                votePoints[(rating.Projekt2) - 1] += 1;
+                votePoints[(rating.Projekt3) - 1] += 1;
+                votePoints[(rating.Projekt4) - 1] += 1;
+                votePoints[(rating.Projekt5) - 1] += 1;
+                votePoints[(rating.Projekt6) - 1] += 1;
+            }
+
+            Console.WriteLine("\nVotes:");
+            for (int i = 1; i < count; i++)
+            {
+                Console.WriteLine($"{i + 1}: {votePoints[i]} Votes");
+            }
+
+            int maxPoints = votePoints[0];
+
+            for (int i = 0; i < count; i++)
+            {
+                if (votePoints[i] > maxPoints) maxPoints = votePoints[i];
+            }
+
+            Console.WriteLine($"\nMaximalpunkte: {maxPoints} Punkte");
+
+            double[] totalPoints = new double[count];
+            Console.WriteLine("\nPunkte:");
+
+            for (int i = 0; i < count; i++)
+            {
+                totalPoints[i] = (votePoints[i] * 30.0) / maxPoints;
+                Console.WriteLine($"{i + 1}: {totalPoints[i]} Punkte");
+            }
+
+            DbWrapperMySql wrappr = DbWrapperMySql.Wrapper;
+
+            string sql;
+            int errorCount = 0;
+
+            for (int i = 1; i <= count; i++)
+            {
+                Console.WriteLine(i);
+                sql = $"UPDATE {table_friendsOfAward_Projects} SET Juryvoting = '{totalPoints[i - 1]}' WHERE Title = '{projectNames[i]}'";
+                try
+                {
+                    wrappr.RunNonQuery(sql);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    errorCount++;
+                }
+            }
+
+            if (errorCount > 0)
+            {
+                errorMessage = $"Erfolgreich eingefügt: {count - errorCount} / Fehler: {errorCount}";
+            }
+
+            return errorMessage;
+        }
     }
 }
